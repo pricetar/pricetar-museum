0bda01b8a291fdb9b44ed13edd929d135ebc11ac
大西泰生 <cpz-user@ONISHI-MacBookPro-16-C02CPNR7MD6M.local>
2021-12-09 15:44:53 +0900

    鳥の無駄な挙動を削除

--- js/list.js
+++ js/list.js
@@ -1,19 +1,45 @@
 'use strict';
 
 {
+    // モーダルに関する処理
     $(function() {
-        $('.nav__item').on('click', function() {
-            //  ボタンをクリックしたら、クリックしたい要素のdata属性を取得
-            let target = $(this).data('modal-link');
-            //  上記で取得した要素と同じclass名を持つ要素を取得
-            let modal = document.querySelector('.' + target);
-            //  その要素にclassを付け替える
-            $(modal).toggleClass('is-show');
+        $('.openModal').click(function(e) { //openModalをクリックした時に
+            var btnIndex = e.currentTarget.accessKey;　 // imgに指定したaccesskeyを参照
+            console.log(e.currentTarget.accessKey); // デバッグ
+            $('.modalArea').eq(btnIndex - 1).addClass('is-open');
+            //クリックしたモーダルボタンと同じ番目のモーダルを表示する。addClassでis-openクラスを追加して表示する
+            　　　　
+            // $('html, body').css('overflow', 'hidden');
+            // overflow:hidden;の追加で背景のスクロール禁止に
+        });
+        $('.closeModal, .modalBg').click(function() { //closeModalかmodalBgをクリックした時に
+            $('.modalArea').removeClass('is-open');
+            //モーダルを非表示にする。removeClassでis-openクラスを削除して非表示にする
+            　　　　
+            // $('html, body').removeAttr('style');
+            //追加したoverflow:hidden;を削除
         });
 
-        //  modalを閉じるときの動作
-        $('.modal__bg').on('click', function() {
-            $(this).parents('.modal').toggleClass('is-show');
+    });
+}
+
+{
+    // トップに戻るボタンの処理
+    $(function() {
+        var pagetop = $('#js-pagetop');
+        pagetop.hide();
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 500) {
+                pagetop.fadeIn();
+            } else {
+                pagetop.fadeOut();
+            }
+        });
+        pagetop.click(function() {
+            $('body, html').animate({
+                scrollTop: 0
+            }, 500);
+            return false;
         });
     });
 }
--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 'use strict';
 
-// {
-//     const bird2 = document.getElementById('bird2');
-//     const bird3 = document.getElementById('bitd3');
 
-//     window.onload = function() { // 画面を開いた瞬間に発火させる関数
-//         setTimeout(function() {
-//             bird2.classList.add("bird");
-//             bird3.classList.add("bird");
-//         }, 4000);
-//     }
-// }
+// 鳥の無駄な挙動対応
+const bird2 = document.getElementById('bird2');
+const bird3 = document.getElementById('bird3');
+
+const remove1 = () => {
+    bird2.classList.remove('opa1');
+}
+const remove2 = () => {
+        bird3.classList.remove('opa1');
+    }
+    //1,000ミリ秒後にcountUp関数を呼び出す処理を追加
+setTimeout(remove1, 2000);
+setTimeout(remove2, 3000);
