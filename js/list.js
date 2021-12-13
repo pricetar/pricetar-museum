b2a2c24e4ac051bb933c4c17828b375be50dda8b
大西泰生 <cpz-user@ONISHI-MacBookPro-16-C02CPNR7MD6M.local>
2021-12-13 14:21:54 +0900

    背景のカーテンの大きさを変更

--- js/list.js
+++ js/list.js
@@ -8,16 +8,10 @@
             console.log(e.currentTarget.accessKey); // デバッグ
             $('.modalArea').eq(btnIndex - 1).addClass('is-open');
             //クリックしたモーダルボタンと同じ番目のモーダルを表示する。addClassでis-openクラスを追加して表示する
-            　　　　
-            // $('html, body').css('overflow', 'hidden');
-            // overflow:hidden;の追加で背景のスクロール禁止に
         });
         $('.closeModal, .modalBg').click(function() { //closeModalかmodalBgをクリックした時に
             $('.modalArea').removeClass('is-open');
             //モーダルを非表示にする。removeClassでis-openクラスを削除して非表示にする
-            　　　　
-            // $('html, body').removeAttr('style');
-            //追加したoverflow:hidden;を削除
         });
 
     });
